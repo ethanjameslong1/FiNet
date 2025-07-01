6d9569c43795899a0fdaec95085999b22e6c88e9
ethanjameslong1 <el23t@fsu.edu>
2025-07-01 17:34:50 -0400

    Fixed database locations

--- database/stock.go
+++ database/stock.go
@@ -10,5 +10,5 @@ import (
 )
 
 const (
-	StockDataSource string = "ethan:040323@tcp(10.0.0.89:3308)/my_go_db"
+	StockDataSource string = "ethan:040323@tcp(stock_prediction_db:3306)/stock_prediction_db?parseTime=true"
 )
