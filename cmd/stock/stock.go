0ee7bc2ce6bdb6b4d315f78b13504fdee537c930
ethanjameslong1 <el23t@fsu.edu>
2025-09-18 16:59:23 -0400

    Refactor IP, stuck on import syntax for decoupled containers

---
+++ cmd/stock/stock.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/ethanjameslong1/FiNet/database"
+	"github.com/ethanjameslong1/FiNet/handler"
+)
+
+func main() {
+	for true {
+		println("Still alive")
+	}
+}
