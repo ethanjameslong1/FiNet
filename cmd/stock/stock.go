a106d3ee87fab90ad3f9f44b78a8ac69c628cd53
ethanjameslong1 <el23t@fsu.edu>
2025-09-30 15:04:00 -0400

    Decouple stock service and add analysis handler

--- cmd/stock/stock.go
+++ cmd/stock/stock.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/ethanjameslong1/FiNet/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start server on port 8080 (default)
+	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
+	r.Run()
 }
